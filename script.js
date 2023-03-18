d2e1439616d777c394fd3cf831a883dcaae7bc35
Dheeraj Singh <112228200+dhrax21@users.noreply.github.com>
2023-03-18 07:31:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+const gridItems = document.querySelectorAll('.grid-item');
+
+        function changeColor(block_id, colour) {
+            const block = document.getElementById(block_id);
+            block.style.backgroundColor = colour;
+        }
+
+        function resetColors() {
+            gridItems.forEach((item) => {
+                item.style.backgroundColor = 'transparent';
+            });
+        }
+
+        document.getElementById('change-button').addEventListener('click', () => {
+            const block_id = document.getElementById('block_id').value;
+            const colour_id = document.getElementById('colour_id').value;
+            const colour = document.getElementById(colour_id).value;
+
+            resetColors();
+            changeColor(block_id, colour);
+        });
+
+        document.getElementById('reset-button').addEventListener('click', () => {
+            resetColors();
+        });
